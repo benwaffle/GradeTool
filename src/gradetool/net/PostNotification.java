bf5e0e3f410f3ecde4065a072f709f7f25a0e6e4
Ben Iofel <iofelben@gmail.com>
2014-03-23 10:59:16 -0400

    update

--- src/gradetool/net/PostNotification.java
+++ src/gradetool/net/PostNotification.java
@@ -1,24 +1,29 @@
 package gradetool.net;
 
+import gradetool.data.Assignment;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public class PostNotification {
 	public static void main(String[] args) {
-		addToTwilioQueue("phone", "body", new Date(12));
-		System.out.println("Post made.");
+		addToTwilioQueue("+12018882057", "testing", Calendar.getInstance().getTime());
+	}
+	
+	public static void addToTwilioQueue(Assignment ass){
+		addToTwilioQueue("2018882057", "This is due tomorrow!!!\n" + ass.getTitle(), ass.getDueDate());
 	}
 	
 	public static void addToTwilioQueue(String phone, String body, Date when) {
 		try {
 			String query = String.format(
 				"notif_due=%s&notif_phone=%s&notif_body=%s", 
-				URLEncoder.encode(when.toString(), "UTF-8"),
+				URLEncoder.encode(new java.sql.Date(when.getTime()).toString(), "UTF-8"),
 				URLEncoder.encode(phone, "UTF-8"),
 				URLEncoder.encode(body, "UTF-8")
 			);
-			URL url = new URL("http://requestb.in/vzcb63vz");
+			URL url = new URL("http://iofel.me/gradetool/post.php");
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("POST");
 			con.setDoOutput(true);
@@ -38,7 +43,10 @@ public class PostNotification {
 			os.close();
 			
 			int resp = con.getResponseCode();
-			System.out.println("Response: "+resp);
+//			System.out.println("Response: "+resp);
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//			for (int i = 0; i < 7; i++)
+//				System.out.println(br.readLine());
 			con.disconnect();
 		} catch (IOException e) {
 			e.printStackTrace();
