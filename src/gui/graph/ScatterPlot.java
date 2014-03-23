69301255c694d93f7f1de9a742f08a27c7bb8243
Prince781 <princetonf@optonline.net>
2014-03-22 21:01:19 -0400

    Update.

--- src/gui/graph/ScatterPlot.java
+++ src/gui/graph/ScatterPlot.java
@@ -6,28 +6,9 @@ package gui.graph;
  */
 public class ScatterPlot extends ChartModel {
 	private String title; // the title of the chart
-	private double[] x, y;
+	private XYData data;
 	
-	/**
-	 * Creates a new <code>ScatterPlot</code>, with title and XY-data.
-	 * @param title The title of the chart.
-	 * @param x x-coordinate data (domain values)
-	 * @param y y-coordinate data (range values)
-	 */
-	public ScatterPlot(String title, double[] x, double[] y) {
-		this.title = title;
-		this.x = x;
-		this.y = y;
-	}
-	public ScatterPlot(String title, double... xy) {
-		double[] t1 = new double[xy.length/2 - xy.length%2];
-		double[] t2 = new double[xy.length/2 - xy.length%2];
-		for (int i=0; i+1<xy.length; i+=2)
-			t1[i/2] = xy[i];
-			t2[i/2] = xy[i+1];
-		this(title, t1, t2);
-	}
-	public void render(Graphics g, DataModel data) {
+	public void render(Graphics g, XYData data) {
 		Graphics2D g2d = (Graphics2D) g;
 		
 	}
