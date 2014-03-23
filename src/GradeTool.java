de06be47c39c8a434cb3e5e2e3155958a0f38f16
Ben Iofel <iofelben@gmail.com>
2014-03-22 21:08:37 -0400

    reset

---
+++ src/GradeTool.java
@@ -0,0 +1,9 @@
+
+
+public class GradeTool {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/TestLoader.java
+++
@@ -1,9 +0,0 @@
-// main class loader
-import gui.*;
-import gui.graph.*;
-
-public class TestLoader {
-	public static void main(String[] args) {
-		MainWindow mw = new MainWindow("Testing", 600, 600);
-	}
-}
