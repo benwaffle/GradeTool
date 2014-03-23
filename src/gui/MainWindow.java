828406a924f84618b1d1a73519f59fb3b7184db5
Ben Iofel <iofelben@gmail.com>
2014-03-22 22:12:25 -0400

    moved toolbar into separate class

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,16 +1,10 @@
 package gui;
 
 import java.awt.*;
-import java.awt.image.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
-	JToolBar toolbar = null;
-	JComboBox<String> userMenu = null;
-	JButton powerschool = null, calendar = null;
-	final int dropDownWidthFrac = 5;
-	
 	private Dimension screen;
 	private Dimension window;
 	
@@ -20,36 +14,7 @@ public class MainWindow extends JFrame {
 		setBounds(100, 100, 800, 600);
 		setResizable(false);
 
-		//dropdown menu
-		userMenu = new JComboBox<String>(new String[] { "Ben Iofel", "Log Out" });
-		Dimension comboBoxWidth = userMenu.getPreferredSize();
-		comboBoxWidth.width = this.getWidth() / dropDownWidthFrac; // quarter of the window
-		userMenu.setMinimumSize(comboBoxWidth);
-		userMenu.setMaximumSize(comboBoxWidth);
-
-		//buttons
-		powerschool = new JButton("PowerSchool");
-		calendar = new JButton("Calendar");
-		
-		//button width
-		Dimension buttonWidth = powerschool.getPreferredSize();
-		buttonWidth.width = this.getWidth() - (this.getWidth() / dropDownWidthFrac) / 2; // half of width
-		
-		//set button size
-		powerschool.setMinimumSize(buttonWidth);
-		powerschool.setMaximumSize(buttonWidth);
-		calendar.setMinimumSize(buttonWidth);
-		calendar.setMaximumSize(buttonWidth);
-
-		//add stuff to toolbar
-		toolbar = new JToolBar(JToolBar.HORIZONTAL);
-		toolbar.setFloatable(false);
-		toolbar.add(userMenu);
-		toolbar.add(powerschool);
-		toolbar.add(calendar);
-
-		//add toolbar to window
-		add(toolbar, BorderLayout.NORTH);
+		add(new MainWindowToolbar(this.getWidth()), BorderLayout.NORTH);
 		
 		window = new Dimension(800, 600);
 		screen = Toolkit.getDefaultToolkit().getScreenSize();
@@ -58,5 +23,4 @@ public class MainWindow extends JFrame {
 			window.width, window.height);
 		setVisible(true);
 	}
-
 }
--- src/gui/MainWindowToolbar.java
+++ src/gui/MainWindowToolbar.java
@@ -1,10 +1,43 @@
 package gui;
 
-import javax.swing.*;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JToolBar;
 
+@SuppressWarnings("serial")
 public class MainWindowToolbar extends JToolBar {
 	JComboBox<String> userMenu = null;
-	JButton powerschool = null, calendar = null;
-	
-	
+	JButton showPSPanel = null, showCalPanel = null;
+
+	public MainWindowToolbar(int parentWidth) {
+		create();
+		setSizes(parentWidth);
+
+		setFloatable(false);
+		add(userMenu);
+		add(showPSPanel);
+		add(showCalPanel);
+	}
+
+	private void create() {
+		userMenu = new JComboBox<String>(
+				new String[] {"Ben Iofel","Log Out" });
+		showPSPanel = new JButton("PowerSchool");
+		showCalPanel = new JButton("Calendar");
+	}
+
+	private void setSizes(int parentWidth) {
+		Dimension menuDim = userMenu.getPreferredSize();
+		menuDim.width = parentWidth / 4;
+		userMenu.setMinimumSize(menuDim);
+		userMenu.setMaximumSize(menuDim);
+		
+		Dimension buttDim = showPSPanel.getPreferredSize();
+		buttDim.width = parentWidth / 4 * 3 / 2;
+		showPSPanel.setMinimumSize(buttDim);
+		showPSPanel.setMaximumSize(buttDim);
+		showCalPanel.setMinimumSize(buttDim);
+		showCalPanel.setMaximumSize(buttDim);
+	}
 }
